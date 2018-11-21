e8ab13afba42bedbad30aedcd01e93893ec46085
FilipeAugustoVO <43390392+FilipeAugustoVO@users.noreply.github.com>
2018-11-21 19:58:46 -0300

    Add files via upload

---
+++ PropriedadesAutoImplementadas2/PropriedadesAutoImplementadas2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PropriedadesAutoImplementadas2
+{
+    public class Movie
+    {
+        public string Titulo { get; set; }
+
+        public string Genero { get; set; }
+
+        public override string ToString()
+        {
+            return Titulo + "\t" + Genero;
+        }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            Movie m = new Movie();
+
+            m.Titulo = "Em busca da felicidade";
+
+            m.Genero = "Drama";
+
+            Console.WriteLine(m);
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
+
